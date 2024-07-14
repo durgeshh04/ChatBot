c552e8a7c265e9ff597f9337b6de1c29912879d3
Durgesh Chaudhari <133797261+durgeshh04@users.noreply.github.com>
2024-07-15 02:11:57 +0530

    Update functionality

--- my-project/src/components/Chat.jsx
+++ my-project/src/components/Chat.jsx
@@ -6,14 +6,14 @@ const Chat = () => {
     <div className="w-screen h-screen bg-gray-900 flex">
       <div className="w-[20%] h-full bg-gray-950 text-white p-4 flex flex-col">
         <div className="h-[5%] mb-4">
-          <button className="w-full h-[50px] border rounded">+ New Chat</button>
+          <button className="w-full h-[50px] border rounded font-bold">+ New Chat</button>
         </div>
         <div className="h-[75%] mt-5 overflow-y-auto scrollbar-hide">
-          <div className="space-y-5">
-            {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13].map((item, index) => (
+          <div className="space-y-3">
+            {[...Array(13).keys()].map((item, index) => (
               <div
                 key={index}
-                className="py-3 rounded-lg text-center font-light flex items-center hover:bg-slate-800 px-8 cursor-pointer"
+                className="py-3 rounded-lg font-light flex items-center hover:bg-slate-800 px-8 cursor-pointer"
               >
                 <span className="mr-2">
                   <svg
@@ -34,17 +34,17 @@ const Chat = () => {
                     <path d="M18 4a3 3 0 0 1 3 3v8a3 3 0 0 1 -3 3h-5l-5 3v-3h-2a3 3 0 0 1 -3 -3v-8a3 3 0 0 1 3 -3h12z" />
                   </svg>
                 </span>
-                My Chat History {item}
+                My Chat History {item + 1}
               </div>
             ))}
           </div>
         </div>
-        <div className="h-[20%] overflow-y-auto scrollbar-hide border-t mt-4">
-          <div className="space-y-5 mt-5">
-            {[1, 2].map((item, index) => (
+        <div className="h-[20%] overflow-none scrollbar-hide border-t-2">
+          <div className="space-y-3 mt-2">
+            {[...Array(2).keys()].map((item, index) => (
               <div
                 key={index}
-                className="py-3 rounded-lg text-center font-light flex items-center px-8 cursor-pointer"
+                className="py-3 rounded-lg font-light flex items-center hover:bg-slate-800 px-8 cursor-pointer"
               >
                 <span className="mr-2">
                   <svg
@@ -54,23 +54,32 @@ const Chat = () => {
                     viewBox="0 0 24 24"
                     fill="none"
                     stroke="currentColor"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    className="icon icon-tabler icons-tabler-outline icon-tabler-message"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    class="icon icon-tabler icons-tabler-outline icon-tabler-adjustments-horizontal"
                   >
                     <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                    <path d="M8 9h8" />
-                    <path d="M8 13h6" />
-                    <path d="M18 4a3 3 0 0 1 3 3v8a3 3 0 0 1 -3 3h-5l-5 3v-3h-2a3 3 0 0 1 -3 -3v-8a3 3 0 0 1 3 -3h12z" />
+                    <path d="M14 6m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
+                    <path d="M4 6l8 0" />
+                    <path d="M16 6l4 0" />
+                    <path d="M8 12m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
+                    <path d="M4 12l2 0" />
+                    <path d="M10 12l10 0" />
+                    <path d="M17 18m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
+                    <path d="M4 18l11 0" />
+                    <path d="M19 18l1 0" />
                   </svg>
                 </span>
-                My Chat History {item}
+                My Chat History {item + 1}
               </div>
             ))}
           </div>
         </div>
       </div>
+
+      {/* Chat Box */}
+
       <div className="w-[80%] h-full bg-gray-900"></div>
     </div>
   );
